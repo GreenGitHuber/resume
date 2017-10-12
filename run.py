adcc293881effd9e7a889a6d768dee3278bec6d5
姜子牙的鱼 <921681087@qq.com>
2017-10-12 18:07:50 +0800

    10-12:完成了列表显示以及页面的跳转

--- run.py
+++ run.py
@@ -4,10 +4,26 @@ import json
 
 app = Flask(__name__)
 
+def read_json_file(file_path):
+	jsonfile = open(file_path,'r+')
+	jsontext = jsonfile.read()
+	data = json.loads(jsontext)
+	return data
+
 @app.route('/')
 def hello_world():
-    #data = read_json_file('static/data/index.json')
-    return render_template('index.html')
+    data = read_json_file('static/data/index.json')
+    return render_template('index.html',data=data)
+
+@app.route('/detail/<string:student_number>')
+def look_details(student_number):
+	data = read_json_file('static/data/index.json')
+	user_data={}
+	for item in data:
+		if student_number == item['student_num']:
+			user_data = item
+			break
+	return render_template('detail.html',data=user_data)
 
 if __name__ == '__main__':
 	# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    
